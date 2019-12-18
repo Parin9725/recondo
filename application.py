b78e031ab0461bf72b4200262adcd15d6e802283
Ubuntu <ubuntu@ip-172-31-1-162.ap-south-1.compute.internal>
2019-12-18 09:32:37 +0000

    dfsg

--- application.py
+++ application.py
@@ -107,7 +107,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Promact</h1>
+    <h1>Vadodara</h1>
     <p>Your Docker Container is now running in Elastic Beanstalk on your own dedicated environment in the AWS Cloud.</p>
   </div>
   
